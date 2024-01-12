b383321543bbb7bb5a905f9595f47dd649f920c7
burcuhasbay <burcu.hasbay@gmail.com>
2024-01-12 13:34:41 +0300

    JDBC- WHILE LOOP for another resultSet

--- src/test/java/JdbcTests/jDBC_examples.java
+++ src/test/java/JdbcTests/jDBC_examples.java
@@ -1,5 +1,6 @@
 package JdbcTests;
 
+
 import org.junit.jupiter.api.Test;
 
 import java.sql.*;
@@ -20,15 +21,28 @@ public class jDBC_examples {
         resultSet.next();
 
         //Display departments table in 10-administration-200-1700
-        System.out.println(resultSet.getString(2));
+       // System.out.println(resultSet.getString(2));
         //HOW CAN I IMPLEMENT THE SAME CODE FOR EACH ROW
         //WHILE LOOP OF COURSE!
+
+        while (resultSet.next()){
+            System.out.println(resultSet.getInt(1)+"-"+(resultSet.getString(2)+"-"+
+        (resultSet.getInt(3)+"-"+(resultSet.getInt(4)))));
+        }
+
+        //YOU DON'T HAVE TO DO THE FIRST 3 THINGS AT THE BEGINNING ALL OVER AGAIN
+        resultSet = statement.executeQuery("SELECT * FROM REGIONS");
+
+        while (resultSet.next()){
+            System.out.println(resultSet.getInt(1)+ "-"+ (resultSet.getString(2)));
+        }
 
         resultSet.close();
         statement.close();
         connection.close();
 
     }
+
 
 
 
